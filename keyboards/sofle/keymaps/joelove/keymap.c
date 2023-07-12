16078637274f10ced7a79a1de1167fb62bacc1ed
Joe Love <git@joelove.uk>
2023-07-12 11:53:36 +0100

    improve encoders

--- keyboards/sofle/keymaps/joelove/keymap.c
+++ keyboards/sofle/keymaps/joelove/keymap.c
@@ -1,23 +1,83 @@
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
-    _COLEMAK,
+    _BASE,
     _SYMBOLS,
-    _NAVIGATE,
-    _ADJUST,
-    _ONE_SHOT,
-    _NUM_PAD,
-    _SYSTEM,
+    _NAVIGATION,
+    _APP_CONTROL,
+    _MODS_NUM_PAD,
 };
 
+/*
+
+0. BASE
+ ␛ q w f p b          j l u y ; [
+ ⌫ a r s t g          m n e i o ⏎
+ ⇥ z x c d v          k h , . / '
+        ➃ ➂ ⇧       ␣ ➀ ➁
+
+1. SYMBOLS
+ £ - = > ? \          ^ { } . + *
+ ⌫ _ ; : , !          ' ( ) " | ␣
+ § @ < $ ` ~          % [ ] / # &
+        ␀ ␀ ␀        ␀ ␀ ␀
+
+2. NAVIGATION
+ ␀ ↞ ↡ ↟ ↠ ␀          ␀ ↥ ↧ ⇞ ⇟ ␀
+ ␀ ⇠ ⇣ ⇡ ⇢ ␀          ␀ ⬅ ⬇ ⬆ ➡ ␀
+ ␀ ⇺ ⇷ ⇸ ⇻ ␀          ␀ ⇱ ⇲ ⍉ ⏏ ␀
+        ␀ ␀ ⇧        ␀ ␀ ␀
+
+3. APP_CONTROL
+ ⟲ ⅰ ⅱ ⅲ ⅳ ⤭          ␀ ␀ ␀ ␀ ␀ ␀
+ ☓ ⤎  ⤏ ⤌  ⤍ ⤢          ␀ ␀ ␀ ␀ ␀ ␀
+ ⟳ ⤎  ⤏   ↜  ↝    ↯          ␀ ␀ ␀ ␀ ␀ ␀
+        ␀ ␀ ␀        ␀ ␀ ␀
+
+4. MODS_NUM_PAD
+ ␀ ␀ ␀ ␀ ␀ ␀          / 7 8 9 - ␀
+ ␀ ⇧   ⌃   ⌥   ⌘ ⌁          * 4 5 6 + ␀
+ ␀ ␀ ␀ ␀ ␀ ␀          = 1 2 3 ⇥ ␀
+        ␀ ␀ ␀        ␣ 0 .
+
+*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_COLEMAK]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_ENT, KC_ESC, KC_Z, KC_X, KC_C, KC_D, KC_V, LGUI(LCTL(KC_Q)), KC_F10, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT, KC_NO, KC_NO, MO(4), MO(3), KC_LSFT, KC_SPC, MO(1), MO(2), KC_NO, KC_NO),
-    [_SYMBOLS]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RSFT(KC_HASH), KC_MINS, KC_EQL, RSFT(KC_GT), RSFT(KC_QUES), KC_BSLS, RSFT(KC_CIRC), RSFT(KC_LCBR), RSFT(KC_RCBR), KC_DOT, RSFT(KC_PLUS), RSFT(KC_ASTR), KC_NUBS, RSFT(KC_UNDS), KC_SCLN, RSFT(KC_COLN), KC_COMM, RSFT(KC_EXLM), KC_QUOT, RSFT(KC_LPRN), RSFT(KC_RPRN), RSFT(KC_DQUO), RSFT(KC_PIPE), RSFT(KC_AMPR), KC_NO, RSFT(KC_AT), RSFT(KC_LT), RSFT(KC_DLR), KC_GRV, RSFT(KC_TILD), KC_NO, KC_NO, RSFT(KC_PERC), KC_LBRC, KC_RBRC, KC_SLSH, RALT(KC_3), KC_NO, KC_NO, KC_NO, TO(6), TO(5), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [_NAVIGATE] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGUI(KC_LEFT), RGUI(KC_DOWN), RGUI(KC_UP), RGUI(KC_RGHT), KC_NO, KC_NO, KC_F1, KC_F2, KC_F11, KC_F12, KC_NO, KC_NO, RALT(KC_LEFT), RALT(KC_DOWN), RALT(KC_UP), RALT(KC_RGHT), KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, LGUI(KC_BSPC), LALT(KC_BSPC), LALT(KC_DEL), LCTL(KC_K), KC_NO, KC_NO, KC_NO, KC_NO, RGUI(KC_MINS), RGUI(KC_EQL), RGUI(KC_0), KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [_ADJUST]   = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, HYPR(KC_1), HYPR(KC_2), HYPR(KC_3), HYPR(KC_4), LGUI(KC_ENT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MEH(KC_GRV), MEH(KC_Z), LCAG(KC_X), LCAG(KC_C), LCAG(KC_SPC), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, HYPR(KC_GRV), HYPR(KC_Z), LCA(KC_X), LCA(KC_C), LAG(KC_TAB), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_SPC), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [_ONE_SHOT] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_HYPR), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [_NUM_PAD]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO, KC_BSPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO, KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PEQL, KC_P1, KC_P2, KC_P3, KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_ENT, KC_P0, KC_PDOT, KC_NO, KC_NO),
-    [_SYSTEM]   = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+    [_BASE] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_ESC, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC,
+        KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_ENT,
+        KC_TAB, KC_Z, KC_X, KC_C, KC_D, KC_V, LGUI(LCTL(KC_Q)), KC_F10, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+        KC_NO, KC_NO, MO(4), MO(3), KC_LSFT, KC_SPC, MO(1), MO(2), KC_NO, KC_NO
+    ),
+    [_SYMBOLS] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        RSFT(KC_HASH), KC_MINS, KC_EQL, RSFT(KC_GT), RSFT(KC_QUES), KC_BSLS, RSFT(KC_CIRC), RSFT(KC_LCBR), RSFT(KC_RCBR), KC_DOT, RSFT(KC_PLUS), RSFT(KC_ASTR),
+        KC_BSPC, RSFT(KC_UNDS), KC_SCLN, RSFT(KC_COLN), KC_COMM, RSFT(KC_EXLM), KC_QUOT, RSFT(KC_LPRN), RSFT(KC_RPRN), RSFT(KC_DQUO), RSFT(KC_PIPE), KC_ENT,
+        KC_NUBS, RSFT(KC_AT), RSFT(KC_LT), RSFT(KC_DLR), KC_GRV, RSFT(KC_TILD), KC_NO, KC_NO, RSFT(KC_PERC), KC_LBRC, KC_RBRC, KC_SLSH, RALT(KC_3), RSFT(KC_AMPR),
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    ),
+    [_NAVIGATION] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, RGUI(KC_LEFT), RGUI(KC_DOWN), RGUI(KC_UP), RGUI(KC_RGHT), KC_NO, KC_NO, KC_F1, KC_F2, KC_F11, KC_F12, KC_NO,
+        KC_NO, RALT(KC_LEFT), RALT(KC_DOWN), RALT(KC_UP), RALT(KC_RGHT), KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
+        KC_NO, LGUI(KC_BSPC), LALT(KC_BSPC), LALT(KC_DEL), LCTL(KC_K), KC_NO, KC_NO, KC_NO, KC_NO, RGUI(KC_MINS), RGUI(KC_EQL), RGUI(KC_0), KC_F10, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    ),
+    [_APP_CONTROL] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        LGUI(KC_Z), HYPR(KC_1), HYPR(KC_2), HYPR(KC_3), HYPR(KC_4), LGUI(KC_ENT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        LCTL(KC_C), MEH(KC_GRV), MEH(KC_Z), LCAG(KC_X), LCAG(KC_C), LCAG(KC_SPC), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        LGUI(LSFT(KC_Z)), HYPR(KC_GRV), HYPR(KC_Z), LCA(KC_X), LCA(KC_C), LAG(KC_TAB), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LGUI(KC_SPC), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    ),
+    [_MODS_NUM_PAD] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO,
+        KC_BSPC, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_HYPR), KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO,
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PEQL, KC_P1, KC_P2, KC_P3, KC_TAB, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SPACE, KC_P0, KC_PDOT, KC_NO, KC_NO
+    )
 };
 
 #ifdef OLED_ENABLE
@@ -45,26 +105,20 @@ static void print_layer_name(void) {
     oled_set_cursor(6, 1);
 
     switch (get_highest_layer(layer_state)) {
-        case _COLEMAK:
+        case _BASE:
             oled_write_P(PSTR("Base Layer"), false);
             break;
         case _SYMBOLS:
             oled_write_P(PSTR("Symbols"), false);
             break;
-        case _NAVIGATE:
-            oled_write_P(PSTR("Navigate"), false);
+        case _NAVIGATION:
+            oled_write_P(PSTR("Navigation"), false);
             break;
-        case _ADJUST:
+        case _APP_CONTROL:
             oled_write_P(PSTR("App Control"), false);
             break;
-        case _ONE_SHOT:
-            oled_write_P(PSTR("One Shot Mods"), false);
-            break;
-        case _NUM_PAD:
-            oled_write_P(PSTR("Number Pad"), false);
-            break;
-        case _SYSTEM:
-            oled_write_P(PSTR("Keyboard Config"), false);
+        case _MODS_NUM_PAD:
+            oled_write_P(PSTR("Mods & Num Pad"), false);
             break;
         default:
             oled_write_P(PSTR("Unknown"), false);
@@ -90,12 +144,16 @@ static void print_mod_status(const char * label, bool on) {
 static void print_mods(void) {
     uint8_t mods = get_mods();
     uint8_t oneshot_mods = get_oneshot_mods();
-    
+
     oled_write_ln_P(PSTR("\n"), false);
+
     print_mod_status(PSTR("Sft"), mods & MOD_MASK_SHIFT || oneshot_mods & MOD_MASK_SHIFT);
     print_mod_status(PSTR("Ctl"), mods & MOD_MASK_CTRL || oneshot_mods & MOD_MASK_CTRL);
     print_mod_status(PSTR("Alt"), mods & MOD_MASK_ALT || oneshot_mods & MOD_MASK_ALT);
     print_mod_status(PSTR("Cmd"), mods & MOD_MASK_GUI || oneshot_mods & MOD_MASK_GUI);
+
+    print_mod_status(PSTR(""), false);
+    print_mod_status(PSTR(""), false);
 }
 
 static void print_divider(void) {
@@ -136,29 +194,50 @@ bool oled_task_user(void) {
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == KC_ESC && record->event.pressed) {
+        uint8_t oneshot_mods = get_oneshot_mods();
+
+        if (oneshot_mods && !has_oneshot_mods_timed_out()) {
+            clear_oneshot_mods();
+            unregister_mods(oneshot_mods);
+            return false;
+        }
+
+        uint8_t oneshot_locked_mods = get_oneshot_locked_mods();
+
+        if (oneshot_locked_mods) {
+            clear_oneshot_locked_mods();
+            unregister_mods(oneshot_locked_mods);
+            return false;
+        }
+    }
+
     return true;
 }
 
 #ifdef ENCODER_ENABLE
 
-bool encoder_update_user(uint8_t index, bool anticlockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        if (anticlockwise) {
-            tap_code(KC_F1);
-            return false;
-        } else {
+        if (clockwise) {
             tap_code(KC_F2);
-            return false;
-        }
-    } else if (index == 1) {
-        if (anticlockwise) {
-            tap_code(KC_VOLD);
-            return false;
         } else {
-            tap_code(KC_VOLU);
-            return false;
+            tap_code(KC_F1);
         }
+
+        return false;
+    } 
+    
+    if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+
+        return false;
     }
+
     return true;
 }
 
