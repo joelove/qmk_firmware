aa8eac1b955e6d98dcb6589f518a2c96a3f750da
Joe Love <git@joelove.uk>
2023-07-20 23:54:20 +0100

    add locks to esc clear

--- keyboards/sofle/keymaps/joelove/keymap.c
+++ keyboards/sofle/keymaps/joelove/keymap.c
@@ -24,8 +24,8 @@ enum sofle_layers {
 
 2. NAVIGATION
  ␀ ↞ ↡ ↟ ↠ ␀          ␀ ↥ ↧ ⇞ ⇟ ␀
- ␀ ⇠ ⇣ ⇡ ⇢ ␀          ␀ ⬅ ⬇ ⬆ ➡ ␀
- ␀ ⇺ ⇷ ⇸ ⇻ ␀          ␀ ⇱ ⇲ ⍉ ⏏ ␀
+ ␀ ⇺ ⇷ ⇸ ⇻ ␀          ␀ ⬅ ⬇ ⬆ ➡ ␀
+ ␀ ⇠ ⇣ ⇡ ⇢ ␀          ␀ ⇱ ⇲ ⍉ ⏏ ␀
         ␀ ␀ ⇧        ␀ ␀ ␀
 
 3. APP_CONTROL
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAVIGATION] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, RGUI(KC_LEFT), RGUI(KC_DOWN), RGUI(KC_UP), RGUI(KC_RGHT), KC_NO, KC_NO, KC_F1, KC_F2, KC_F11, KC_F12, KC_NO,
-        KC_NO, RALT(KC_LEFT), RALT(KC_DOWN), RALT(KC_UP), RALT(KC_RGHT), KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
-        KC_NO, LGUI(KC_BSPC), LALT(KC_BSPC), LALT(KC_DEL), LCTL(KC_K), KC_NO, KC_NO, KC_NO, KC_NO, RGUI(KC_MINS), RGUI(KC_EQL), RGUI(KC_0), KC_F10, KC_NO,
+        KC_NO, LGUI(KC_BSPC), LALT(KC_BSPC), LALT(KC_DEL), LCTL(KC_K), KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
+        KC_NO, RALT(KC_LEFT), RALT(KC_DOWN), RALT(KC_UP), RALT(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_NO, RGUI(KC_MINS), RGUI(KC_EQL), RGUI(KC_0), KC_F10, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     ),
     [_APP_CONTROL] = LAYOUT(
@@ -151,9 +151,6 @@ static void print_mods(void) {
     print_mod_status(PSTR("Ctl"), mods & MOD_MASK_CTRL || oneshot_mods & MOD_MASK_CTRL);
     print_mod_status(PSTR("Alt"), mods & MOD_MASK_ALT || oneshot_mods & MOD_MASK_ALT);
     print_mod_status(PSTR("Cmd"), mods & MOD_MASK_GUI || oneshot_mods & MOD_MASK_GUI);
-
-    print_mod_status(PSTR(""), false);
-    print_mod_status(PSTR(""), false);
 }
 
 static void print_divider(void) {
@@ -193,23 +190,49 @@ bool oled_task_user(void) {
 
 #endif
 
+static bool clear_active_mods(void) {
+    bool mods_cleared = false;
+
+    uint8_t oneshot_mods = get_oneshot_mods();
+
+    if (oneshot_mods && !has_oneshot_mods_timed_out()) {
+        clear_oneshot_mods();
+        unregister_mods(oneshot_mods);
+        mods_cleared = true;
+    }
+
+    uint8_t oneshot_locked_mods = get_oneshot_locked_mods();
+
+    if (oneshot_locked_mods) {
+        clear_oneshot_locked_mods();
+        unregister_mods(oneshot_locked_mods);
+        mods_cleared = true;
+    }
+
+    return mods_cleared;
+}
+
+static void set_default_locks(void) {
+    uint8_t usb_leds = host_keyboard_leds();
+
+    if (usb_leds & (1<<USB_LED_CAPS_LOCK)) {
+        register_code(KC_CAPS_LOCK);
+        unregister_code(KC_CAPS_LOCK);
+    }
+
+    if (~usb_leds & (1<<USB_LED_NUM_LOCK)) {
+        register_code(KC_NUM_LOCK);
+        unregister_code(KC_NUM_LOCK);
+    }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (keycode == KC_ESC && record->event.pressed) {
-        uint8_t oneshot_mods = get_oneshot_mods();
+        bool mods_cleared = clear_active_mods();
 
-        if (oneshot_mods && !has_oneshot_mods_timed_out()) {
-            clear_oneshot_mods();
-            unregister_mods(oneshot_mods);
-            return false;
-        }
+        set_default_locks();
 
-        uint8_t oneshot_locked_mods = get_oneshot_locked_mods();
-
-        if (oneshot_locked_mods) {
-            clear_oneshot_locked_mods();
-            unregister_mods(oneshot_locked_mods);
-            return false;
-        }
+        return !mods_cleared;
     }
 
     return true;
@@ -220,14 +243,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_F2);
+            tap_code(KC_BRIU);
         } else {
-            tap_code(KC_F1);
+            tap_code(KC_BRID);
         }
 
         return false;
-    } 
-    
+    }
+
     if (index == 1) {
         if (clockwise) {
             tap_code(KC_VOLU);
